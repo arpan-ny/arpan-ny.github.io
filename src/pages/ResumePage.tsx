ec0d5f2d1f48f478ee7784cf85e07a0c06c0ded1
Arpan Nyati <nyati.arpan@gmail.com>
2025-05-25 00:31:19 +0530

    .

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -5,13 +5,13 @@ const ResumePage: React.FC = () => {
     <div className="flex-grow flex flex-col items-center justify-center p-4 sm:p-6 mt-20 md:mt-0">
       <h1 className="text-2xl sm:text-4xl font-bold text-gray-800 text-center mb-6">Resume</h1>
       {/* Download Resume Button */}
-      <a
+      {/* <a
         href="/Arpan_Resume.pdf"
         download
         className="mb-6 px-6 py-2 bg-gray-600 text-white rounded shadow hover:bg-gray-700 transition-colors"
       >
         Download Resume (PDF)
-      </a>
+      </a> */}
       {/* Google Docs Resume Button */}
       <a
         href="https://docs.google.com/document/d/1p_xSJnx_m6Qe26CDWyTmXZKnZQDAa9TQ/edit?usp=sharing&ouid=105142803105827680866&rtpof=true&sd=true"
