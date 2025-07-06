694110f6b96ed040fa1016389542bd07be16c8ac
arpan <nyati.arpan@gmail.com>
2025-07-06 22:50:45 +0530

    resume update and download button

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -14,15 +14,27 @@ const ResumePage: React.FC = () => {
       >
         Download Resume (PDF)
       </a> */}
-      {/* Google Docs Resume Button */}
-      <a
-        href="https://docs.google.com/document/d/1p_xSJnx_m6Qe26CDWyTmXZKnZQDAa9TQ/edit?usp=sharing&ouid=105142803105827680866&rtpof=true&sd=true"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="px-6 py-2 bg-gray-600 text-white rounded shadow hover:bg-gray-700 transition-colors mb-6"
-      >
-        View on Google Docs
-      </a>
+      <div className="flex space-x-4 mb-6">
+        {/* Google Docs Resume Button */}
+        <a
+          href="https://drive.google.com/file/d/1JU53_rth5CUlQUszWIiaD2sTb0GxODfv/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="px-6 py-2 bg-gray-600 text-white rounded shadow hover:bg-gray-700 transition-colors flex items-center"
+        >
+          View on Google Docs
+        </a>
+        {/* Download Resume Button */}
+        <a
+          href="/arpan_resume_v1.pdf"
+          download
+          className="p-2 bg-gray-600 text-white rounded shadow hover:bg-gray-700 transition-colors flex items-center"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+          </svg>
+        </a>
+      </div>
     </div>
   );
 };
