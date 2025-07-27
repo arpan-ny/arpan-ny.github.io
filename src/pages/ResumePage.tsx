4ce467713d6a223eef1e96670325347f8b93aaf1
arpan <nyati.arpan@gmail.com>
2025-07-27 10:49:51 +0530

    drive link update

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -17,7 +17,7 @@ const ResumePage: React.FC = () => {
       <div className="flex space-x-4 mb-6">
         {/* Google Docs Resume Button */}
         <a
-          href="https://drive.google.com/file/d/16DgxvO_Xzw-HqWSlcLj1lP-iBD4HlQNe/view?usp=sharing"
+          href="https://drive.google.com/file/d/1AMYtQ96NLmYhg36WIm76j0HqJsDxGG7U/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
           className="px-6 py-2 bg-gray-600 text-white rounded shadow hover:bg-gray-700 transition-colors flex items-center"
